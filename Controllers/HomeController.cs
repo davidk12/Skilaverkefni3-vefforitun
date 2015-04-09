f3a1df46a95258a5e2de5fca928007380a4a8f2c
davidk12 <davidk12@ru.is>
2015-04-09 12:38:56 +0000

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using project_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace project_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            newsRepository repository = new newsRepository();                  //Get the 10 latest entries from the database
+            var blogEntries = repository.getLatestBlogs();                     //and return the view.
+            return View(blogEntries);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id != null)
+            {
+                newsRepository repository = new newsRepository();          
+                var model = repository.get_blog_by_id(id);                      //Get the entry by it's ID  
+                                                                                //And return the view.
+                return View(model);
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection formData)               
+        {
+            newsRepository repository = new newsRepository();               
+            Blog b = repository.get_blog_by_id(id);                             //Get the entry by it's ID
+            if (b != null)                                                      //Check if it exists, if it does
+            {                                                                   //try to edit it, if a form is empty 
+                try                                                             //then return a view which includes a error message.
+                {
+                    UpdateModel(b);
+                    repository.edit_blog(b);
+                    return RedirectToAction("Index");
+                }
+                catch (SqlException)
+                {
+                    return View("EditFail");
+                }
+            }
+            else                                                               //If the entry does not exist, then return a NotFound view
+            {                                                                  
+                return View("NotFound");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()                                         
+        {
+            return View(new Blog());
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection formData)                    //Try to add a entry, if any of it's forms is empty
+        {                                                                      //then catch the exception and return a CreateFail view.
+            try
+            {
+                Blog b = new Blog();
+                UpdateModel(b);
+                newsRepository repository = new newsRepository();
+                repository.add_blog(b);
+                return RedirectToAction("Index");
+            }
+            catch (SqlException)
+            {
+                return View("CreateFail");
+            }
+        }
+    }
+}
---
+++ Models/newsRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Linq;
+using System.Data.SqlClient;
+
+namespace project_3.Models
+{
+    public class newsRepository
+    {
+        private List<Blog> blogs = new List<Blog>();            //The list which holds the 10 latest blogs.
+
+        private blogDBDataContext db = new blogDBDataContext(); //db will give a way to access the database through LINQ.
+
+        public IEnumerable<Blog> getLatestBlogs()               //Query the database for the 10 latest entries,
+        {                                                       //and order the result by their date.
+            var result = (from b in db.Blogs
+                          orderby b.blogDate descending
+                          select new { b.blogID, b.title, b.content, b.blogDate, b.category }).Take(10);
+
+            foreach (var r in result)
+            {
+                Blog blog = new Blog()
+                {
+                    blogID = r.blogID,
+                    title = r.title,
+                    content = r.content,
+                    blogDate = r.blogDate,
+                    category = r.category
+                };
+                blogs.Add(blog);
+            }
+            return blogs;
+        }
+
+        public Blog get_blog_by_id(int? id)
+        {
+            var result = db.Blogs.FirstOrDefault(r => r.blogID == id);                  //Query the database for an entry which matches
+            return result;                                                               //the id parameter.
+        }
+
+        public void add_blog(Blog b)
+        {
+            b.blogDate = DateTime.Now;                                                   
+            db.Blogs.InsertOnSubmit(b);
+
+            db.SubmitChanges();
+        }
+
+        public void edit_blog(Blog b)
+        {
+            var result = get_blog_by_id(b.blogID);              
+            result.title = b.title;
+            result.content = b.content;
+            result.category = b.category;
+
+            db.SubmitChanges();
+        }
+    }
+}
